f740343acb2e5b61e9788abf5c7f37c2acfb2994
Genady Buchatsky <genady.buchatsky@gmail.com>
2022-06-27 13:38:45 +0200

    perf: add synchronization logic

--- AdjustAdobeExtension/Classes/AdjustAdobeExtension.m
+++ AdjustAdobeExtension/Classes/AdjustAdobeExtension.m
@@ -16,6 +16,7 @@ NSString * const ADJAdobeExtensionSdkPrefix = @"adobe_ext1.0.4";
 NSString * const ADJAdobeAdjustEventToken = @"adj.eventToken";
 NSString * const ADJAdobeAdjustEventCurrency = @"adj.currency";
 NSString * const ADJAdobeAdjustEventRevenue = @"adj.revenue";
+char * const kQUEUE_ID_SYNC = "com.adjust.AdjustAdobeExtension.sync_queue";
 
 static AdjustAdobeExtensionConfig *_configInstance = nil;
 
@@ -23,150 +24,11 @@ static AdjustAdobeExtensionConfig *_configInstance = nil;
 
 @property (nonatomic, assign) BOOL sdkInitialized;
 @property (nonatomic, strong) NSMutableArray<NSDictionary *> *receivedEvents;
+@property (nonatomic, strong) dispatch_queue_t syncQueue;
 
 @end
 
 @implementation AdjustAdobeExtension
-
-- (instancetype)init {
-    self = [super init];
-    if (self == nil) {
-        return nil;
-    }
-        
-    _sdkInitialized = NO;
-    _receivedEvents = [NSMutableArray array];
-
-    NSError *error = nil;
-
-    // Shared State listener
-    if ([self.api registerListener:[AdjustAdobeExtensionSharedStateListener class]
-                         eventType:ADJAdobeEventTypeHub
-                       eventSource:ADJAdobeEventSourceSharedState
-                             error:&error]) {
-        [ACPCore log:ACPMobileLogLevelDebug
-                 tag:ADJAdobeExtensionLogTag
-             message:@"Successfully registered Extension Listener for Event Hub Shared State events."];
-    } else {
-        [ACPCore log:ACPMobileLogLevelError
-                 tag:ADJAdobeExtensionLogTag
-             message:[NSString stringWithFormat:
-                      @"An error occured while registering Extension Listener"
-                      " for Event Hub Shared State events. %@",
-                      (error) ? [NSString stringWithFormat:@"Code: %ld, Domain: %@, Description: %@.", (long)error.code, error.domain, error.localizedDescription ] :
-                      @"Unknown error."]];
-    }
-
-    if ([self.api registerListener:[AdjustAdobeExtensionEventListener class]
-                         eventType:ADJAdobeEventTypeGenericTrack
-                       eventSource:ADJAdobeEventSourceRequestContent
-                             error:&error]) {
-        [ACPCore log:ACPMobileLogLevelDebug
-                 tag:ADJAdobeExtensionLogTag
-             message:@"Successfully registered Extension Listener for Extension Request Content events"];
-    } else {
-        [ACPCore log:ACPMobileLogLevelError
-                 tag:ADJAdobeExtensionLogTag
-             message:[NSString stringWithFormat:
-                      @"An error occured while registering Extension Listener"
-                      " for Extension Request Content events: %@",
-                      (error) ? [NSString stringWithFormat:@"Code: %ld, Domain: %@, Description: %@.", (long)error.code, error.domain, error.localizedDescription ] :
-                      @"Unknown error."]];
-    }
-
-    return self;
-}
-
-- (void)handleEventData:(nullable NSDictionary *)eventData {
-    if (eventData == nil) {
-        [ACPCore log:ACPMobileLogLevelError
-                 tag:ADJAdobeExtensionLogTag
-             message:@"Extension event error: eventData is nil!"];
-        return;
-    }
-    
-    NSDictionary *contextdata = eventData[@"contextdata"];
-    if (contextdata == nil) {
-        [ACPCore log:ACPMobileLogLevelError
-                 tag:ADJAdobeExtensionLogTag
-             message:@"Extension event error: contextdata is nil!"];
-        return;
-    }
-
-    NSString *adjEventToken = contextdata[ADJAdobeAdjustEventToken];
-    if (adjEventToken == nil) {
-        return;
-    }
-
-    if (!self.sdkInitialized) {
-        [self.receivedEvents addObject:eventData];
-        return;
-    }
-
-    ADJEvent *event = [ADJEvent eventWithEventToken:adjEventToken];
-    NSString *currency = contextdata[ADJAdobeAdjustEventCurrency];
-    NSNumber *revenue = contextdata[ADJAdobeAdjustEventRevenue];
-
-    if (currency && revenue) {
-        [event setRevenue:[revenue doubleValue] currency:currency];
-    }
-
-    [Adjust trackEvent:event];
-}
-
-- (void)setupAdjustWithAppToken:(NSString *)appToken trackAttribution:(BOOL)trackAttribution {
-    if (!_configInstance) {
-        [ACPCore log:ACPMobileLogLevelError
-                 tag:ADJAdobeExtensionLogTag
-             message:@"Extension should be registered first!"];
-        return;
-    }
-
-    if (self.sdkInitialized) {
-        return;
-    }
-
-    _configInstance.shouldTrackAttribution = trackAttribution;
-
-    ADJConfig *adjustConfig = [ADJConfig configWithAppToken:appToken
-                                                environment:_configInstance.environment];
-    [adjustConfig setSdkPrefix:ADJAdobeExtensionSdkPrefix];
-    [adjustConfig setDelegate:self];
-
-    switch ([ACPCore logLevel]) {
-        case ACPMobileLogLevelError:
-            [adjustConfig setLogLevel:ADJLogLevelError];
-            break;
-        case ACPMobileLogLevelWarning:
-            [adjustConfig setLogLevel:ADJLogLevelWarn];
-            break;
-        case ACPMobileLogLevelDebug:
-            [adjustConfig setLogLevel:ADJLogLevelDebug];
-            break;
-        case ACPMobileLogLevelVerbose:
-            [adjustConfig setLogLevel:ADJLogLevelVerbose];
-            break;
-    }
-
-    [Adjust appDidLaunch:adjustConfig];
-
-    self.sdkInitialized = YES;
-    [self dumpReceivedEvents];
-}
-
-- (void)dumpReceivedEvents {
-    if (self.receivedEvents.count <= 0) {
-        return;
-    }
-
-    // dump events received before initialization
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
-        for (NSDictionary *event in self.receivedEvents) {
-            [self handleEventData:event];
-        }
-        [self.receivedEvents removeAllObjects];
-    });
-}
 
 + (void)registerExtensionWithConfig:(AdjustAdobeExtensionConfig *)config {
 
@@ -193,12 +55,151 @@ static AdjustAdobeExtensionConfig *_configInstance = nil;
     } else {
         [ACPCore log:ACPMobileLogLevelError
                  tag:ADJAdobeExtensionLogTag
-            // message:[NSString stringWithFormat:@"Error registering AdjustExtension: %@ %d",
              message:[NSString stringWithFormat:
                       @"An error occured while registering Adjust Extension: %@",
                       (error) ? [NSString stringWithFormat:@"Code: %ld, Domain: %@, Description: %@.", (long)error.code, error.domain, error.localizedDescription ] :
                       @"Unknown error."]];
     }
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self == nil) {
+        return nil;
+    }
+        
+    _sdkInitialized = NO;
+    _receivedEvents = [NSMutableArray array];
+    _syncQueue = dispatch_queue_create(kQUEUE_ID_SYNC, DISPATCH_QUEUE_SERIAL);
+
+    NSError *error = nil;
+
+    // Shared State listener
+    if ([self.api registerListener:[AdjustAdobeExtensionSharedStateListener class]
+                         eventType:ADJAdobeEventTypeHub
+                       eventSource:ADJAdobeEventSourceSharedState
+                             error:&error]) {
+        [ACPCore log:ACPMobileLogLevelDebug
+                 tag:ADJAdobeExtensionLogTag
+             message:@"Successfully registered Extension Listener for Event Hub Shared State events."];
+    } else {
+        [ACPCore log:ACPMobileLogLevelError
+                 tag:ADJAdobeExtensionLogTag
+             message:[NSString stringWithFormat:
+                      @"An error occured while registering Extension Listener"
+                      " for Event Hub Shared State events. %@",
+                      (error) ? [NSString stringWithFormat:@"Code: %ld, Domain: %@, Description: %@.", (long)error.code, error.domain, error.localizedDescription ] :
+                      @"Unknown error."]];
+    }
+
+    // Events listener
+    if ([self.api registerListener:[AdjustAdobeExtensionEventListener class]
+                         eventType:ADJAdobeEventTypeGenericTrack
+                       eventSource:ADJAdobeEventSourceRequestContent
+                             error:&error]) {
+        [ACPCore log:ACPMobileLogLevelDebug
+                 tag:ADJAdobeExtensionLogTag
+             message:@"Successfully registered Extension Listener for Extension Request Content events"];
+    } else {
+        [ACPCore log:ACPMobileLogLevelError
+                 tag:ADJAdobeExtensionLogTag
+             message:[NSString stringWithFormat:
+                      @"An error occured while registering Extension Listener"
+                      " for Extension Request Content events: %@",
+                      (error) ? [NSString stringWithFormat:@"Code: %ld, Domain: %@, Description: %@.", (long)error.code, error.domain, error.localizedDescription ] :
+                      @"Unknown error."]];
+    }
+
+    return self;
+}
+
+- (void)setupAdjustWithAppToken:(NSString *)appToken trackAttribution:(BOOL)trackAttribution {
+
+    dispatch_async(self.syncQueue, ^{
+
+        if (!_configInstance) {
+            [ACPCore log:ACPMobileLogLevelError
+                     tag:ADJAdobeExtensionLogTag
+                 message:@"Extension should be registered first!"];
+            return;
+        }
+
+        if (self.sdkInitialized == YES) {
+            return;
+        }
+
+        self.sdkInitialized = YES;
+
+        _configInstance.shouldTrackAttribution = trackAttribution;
+        ADJConfig *adjustConfig = [ADJConfig configWithAppToken:appToken
+                                                    environment:_configInstance.environment];
+        [adjustConfig setSdkPrefix:ADJAdobeExtensionSdkPrefix];
+        [adjustConfig setDelegate:self];
+
+        switch ([ACPCore logLevel]) {
+            case ACPMobileLogLevelError:
+                [adjustConfig setLogLevel:ADJLogLevelError];
+                break;
+            case ACPMobileLogLevelWarning:
+                [adjustConfig setLogLevel:ADJLogLevelWarn];
+                break;
+            case ACPMobileLogLevelDebug:
+                [adjustConfig setLogLevel:ADJLogLevelDebug];
+                break;
+            case ACPMobileLogLevelVerbose:
+                [adjustConfig setLogLevel:ADJLogLevelVerbose];
+                break;
+        }
+
+        [Adjust appDidLaunch:adjustConfig];
+
+        for (NSDictionary *event in self.receivedEvents) {
+            [self processEvent:event];
+        }
+        [self.receivedEvents removeAllObjects];
+    });
+}
+
+- (void)handleEventData:(nullable NSDictionary *)eventData {
+    dispatch_async(self.syncQueue, ^{
+        if (!self.sdkInitialized) {
+            [self.receivedEvents addObject:eventData];
+        } else {
+            [self processEvent:eventData];
+        }
+    });
+}
+
+- (void)processEvent:(nullable NSDictionary *)eventData {
+
+    if (eventData == nil) {
+        [ACPCore log:ACPMobileLogLevelError
+                 tag:ADJAdobeExtensionLogTag
+             message:@"Extension event error: eventData is nil!"];
+        return;
+    }
+
+    NSDictionary *contextdata = eventData[@"contextdata"];
+    if (contextdata == nil) {
+        [ACPCore log:ACPMobileLogLevelError
+                 tag:ADJAdobeExtensionLogTag
+             message:@"Extension event error: contextdata is nil!"];
+        return;
+    }
+
+    NSString *adjEventToken = contextdata[ADJAdobeAdjustEventToken];
+    if (adjEventToken == nil) {
+        return;
+    }
+
+    ADJEvent *event = [ADJEvent eventWithEventToken:adjEventToken];
+    NSString *currency = contextdata[ADJAdobeAdjustEventCurrency];
+    NSNumber *revenue = contextdata[ADJAdobeAdjustEventRevenue];
+
+    if (currency && revenue) {
+        [event setRevenue:[revenue doubleValue] currency:currency];
+    }
+    [Adjust trackEvent:event];
 }
 
 - (nullable NSString *)name {
