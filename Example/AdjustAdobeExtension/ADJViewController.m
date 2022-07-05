d94d5ae72be25bbbd364ddd756c6269dcc92b052
Genady Buchatsky <genady.buchatsky@gmail.com>
2022-07-05 13:25:11 +0200

    feat: add push token, track callback/partner params

--- Example/AdjustAdobeExtension/ADJViewController.m
+++ Example/AdjustAdobeExtension/ADJViewController.m
@@ -19,14 +19,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    [ACPCore trackAction:@"TestAction" data:@{@"a": @"b",
-                                              ADJAdobeAdjustEventToken: @"g3mfiw"}];
-    [ACPCore trackAction:@"TestActionRevenue" data:@{@"a": @"b",
-                                                     ADJAdobeAdjustEventToken: @"a4fd35",
-                                                     ADJAdobeAdjustEventRevenue: @"1.0",
-                                                     ADJAdobeAdjustEventCurrency: @"EUR"}];
-    [ACPCore trackState:@"TestState" data:@{@"a": @"b"}];
-    
+    // Track simple event
+    NSMutableDictionary * dataDict = [NSMutableDictionary dictionary];
+    [dataDict setValue:@"g3mfiw" forKey:ADJAdobeAdjustEventToken];
+    [ACPCore trackAction:ADJAdobeAdjustActionTrackEvent data:dataDict];
+
+    [dataDict removeAllObjects];
+
+    // Track Revenue event
+    [dataDict setValue:@"a4fd35" forKey:ADJAdobeAdjustEventToken];
+    [dataDict setValue:@"1.0" forKey:ADJAdobeAdjustEventRevenue];
+    [dataDict setValue:@"EUR" forKey:ADJAdobeAdjustEventCurrency];
+    [ACPCore trackAction:ADJAdobeAdjustActionTrackEvent data:dataDict];
+
+    [dataDict removeAllObjects];
+
+    // Track event with Callback parameters
+    [dataDict setValue:@"34vgg9" forKey:ADJAdobeAdjustEventToken];
+    [dataDict setValue:@"value1" forKey:[ADJAdobeAdjustEventCallbackParamPrefix stringByAppendingString:@"key1"]];
+    [dataDict setValue:@"value2" forKey:[ADJAdobeAdjustEventCallbackParamPrefix stringByAppendingString:@"key2"]];
+    [ACPCore trackAction:ADJAdobeAdjustActionTrackEvent data:dataDict];
+
+    [dataDict removeAllObjects];
+
+    // Track event with Partner parameters
+    [dataDict setValue:@"w788qs" forKey:ADJAdobeAdjustEventToken];
+    [dataDict setValue:@"value1" forKey:[ADJAdobeAdjustEventPartnerParamPrefix stringByAppendingString:@"key1"]];
+    [dataDict setValue:@"value2" forKey:[ADJAdobeAdjustEventPartnerParamPrefix stringByAppendingString:@"key2"]];
+    [ACPCore trackAction:ADJAdobeAdjustActionTrackEvent data:dataDict];
+
+    [dataDict removeAllObjects];
+
+    // Set Push Token
+    [dataDict setValue:@"your_app_push_token" forKey:ADJAdobeAdjustPushToken];
+    [ACPCore trackAction:ADJAdobeAdjustActionSetPushToken data:dataDict];
 }
 
 - (void)didReceiveMemoryWarning {
